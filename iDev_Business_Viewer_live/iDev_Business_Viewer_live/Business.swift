7fa1806dbe16cd34147d0a09f6fa185d413a360a
Deetpanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-01-13 20:44:36 -0500

    Added NSObject

---
+++ iDev_Business_Viewer_live/iDev_Business_Viewer_live/Business.swift
@@ -0,0 +1,13 @@
+//
+//  Business.swift
+//  iDev_Business_Viewer_live
+//
+//  Created by Deetpanshu Malik on 1/13/17.
+//  Copyright © 2017 Deetpanshu Malik. All rights reserved.
+//
+
+import UIKit
+
+class Business: NSObject {
+
+}
